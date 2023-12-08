36b190bb51f2ee41c16b4c6d0b74ac45cd588332
DarkElf2233 <fd.melnikoff@gmail.com>
2023-12-08 14:16:00 +0300

    Add babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        library: 'adv-js-hw3_1',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+        globalObject: 'this',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+};
